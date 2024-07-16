ffcd9e2c44b5e0760b7a15f7d8d7271e48ed4c83
Mike <18002494+mikeunge@users.noreply.github.com>
2024-07-16 10:08:42 +0200

    fix: set everforest as default, update some auto setups

--- lua/plugins/color-deepwhite.lua
+++ lua/plugins/color-deepwhite.lua
@@ -2,7 +2,4 @@ return {
   "Verf/deepwhite.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme deepwhite]])
-  end,
 }
--- lua/plugins/color-miasma.lua
+++ lua/plugins/color-miasma.lua
@@ -2,7 +2,4 @@ return {
   "xero/miasma.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd("colorscheme miasma")
-  end,
 }
